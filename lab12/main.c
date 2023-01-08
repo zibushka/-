cd0d6ac319a6c48688ac578967e5e72ebb4cb704
zibushka <113304641+zibushka@users.noreply.github.com>
2023-01-08 17:23:30 +0300

    Update main.c

--- lab12/main.c
+++ lab12/main.c
@@ -1,58 +1,71 @@
 #include <stdio.h>
-#include <conio.h>
+#include <math.h>
 int main(){
-	int i, d = 10;
-	char mas[d];
-	scanf("%s", mas);
-	for(i=0;i<d;i=i+1){
-		if (mas[i]=='0'){
-		    mas[i]='9';
-		}
-		else{
-		    if (mas[i]=='9'){
-		        mas[i]='0';
-		    }
-		    
-		}
-		if (mas[i]=='1'){
-		    mas[i]='8';
-		}
-		else{
-		    if (mas[i]=='8'){
-		        mas[i]='1';
-		    }
-		    
-		}
-		if (mas[i]=='2'){
-		    mas[i]='7';
-		}
-		else{
-		    if (mas[i]=='7'){
-		        mas[i]='2';
-		    }
-		    
-		}
-		if (mas[i]=='3'){
-		    mas[i]='6';
-		}
-		else{
-		    if (mas[i]=='6'){
-		        mas[i]='3';
-		    }
-		    
-		}
-		if (mas[i]=='4'){
-		    mas[i]='5';
-		}
-		else{
-		    if (mas[i]=='5'){
-		        mas[i]='4';
-		    }
-		    
-		}
-		
+	int ch0, ch, ch1, new = 0, f, k = 0;
+	scanf("%d\n", &ch);
+	ch0 = ch;
+	for (int i; ch != 0; k = k + 1 ){
+	    ch1 = ch % 10;
+
+	    if (ch >= 1){
+	        ch = ch - ch1;
+	        ch = ch / 10;
+	    }
 	}
-	mas[d]='\0';
-	puts(mas);
-	getch();
+	int s = 0;
+	for (int i; k!= 0; k = k - 1){
+	    ch1 = ch0 % 10;
+	    
+	    if (ch1==0){
+		    new = new + 9*pow(10, s);
+		}
+		else{
+		    if (ch1==9){
+		        new = new + 0*pow(10, s);
+		    }
+		    
+		}
+		if (ch1==1){
+		    new = new + 8*pow(10, s);
+		}
+		else{
+		    if (ch1==8){
+		        new = new + 1*pow(10, s);
+		    }
+		    
+		}
+		if (ch1==2){
+		    new = new + 7*pow(10, s);
+		}
+		else{
+		    if (ch1==7){
+		        new = new + 2*pow(10, s);
+		    }
+		    
+		}
+		if (ch1==3){
+		    new = new + 6*pow(10, s);
+		}
+		else{
+		    if (ch1==6){
+		        new = new + 3*pow(10, s);
+		    }
+		    
+		}
+		if (ch1==4){
+		    new = new + 5*pow(10, s);
+		}
+		else{
+		    if (ch1==5){
+		        new = new + 4*pow(10, s);
+		    }
+		    
+		}
+		s = s + 1;
+		ch0 = ch0 - ch1;
+		ch0 = ch0 / 10;
+	}
+	printf("%d\n", new);
 }
+	
+
